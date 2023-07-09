6a865a01135786622d122a99a8e97d46b7afc179
Grong <lionel.de-sousa@epitech.eu>
2023-07-09 03:24:50 +0200

    [ADD] bdd + main now is infinite [UPD]  menu + data to bdd

--- minipro/main.py
+++ minipro/main.py
@@ -5,23 +5,17 @@ import os
 import sql.bdd_request as sql
 
 class classe():
-    
-    __students = []
-    
+
     def __init__(self, name, average = 0):
-        self.name = name
-        self.average = average
-        sql.create_classe()
-    
-    def getStudentsList(self):
-        return self.__students
-    
+        self.__name = name
+        self.__average = average
+        sql.insert_in_classe(self.__name)
+
     def add_student(self, student):
-        stud = {"name": student.get_name(), "id": student.get_id(), "date_of_birth": student.get_date_of_birth(), "mark": student.get_mark()}
-        print(sql.get_student(stud["id"]))
-        #sql.insert_in_student(sql.get_student(stud["id"]), )
-        self.__students.append(stud)
-    
+        sql.update_in_student("classe", sql.get_classe_id(str(self.__name))[0], student.get_matricule())
+        self.calcul_moyenne()
+        sql.update_in_classe("average_mark", int(self.__average), str(self.__name))
+
     def remove_student(self, student):
         for i in range(len(self.__students)):
             if self.__students[i]['id'] == student.get_id():
@@ -29,17 +23,137 @@ class classe():
                 break
 
     def calcul_moyenne(self):
-        for i in range(len(self.__students)):
-            #print(self.__students[i]['mark'])
-            self.average += int(self.__students[i]['mark'])
-        self.average /= len(self.__students)
+        self.__average = 0
+        students = sql.get_all_students_from_classe(sql.get_classe_id(str(self.__name))[0])
+        for i in range(len(students)):
+            self.__average += int(students[i][4])
+        self.__average /= len(students)
 
-    def print_average(self):
-        os.system('cls')
-        print("Moyenne: ", self.average)
-        print("Nombre d'étudiants: ", len(self.__students))
+    def get_name(self):
+        return self.__name
 
-    def modify_student(self, student):
+    def set_name(self, name):
+        self.__name = name
+        return self.__name   
+
+    def get_average(self):
+        return self.__average
+
+    def set_name(self, name):
+        self.__name = name
+        return self.__name
+
+from abc import ABC, abstractmethod
+class person(ABC):
+
+    def __init__(self, name, date_of_birth):
+        self.name = name
+        self.date_of_birth = date_of_birth
+
+    @abstractmethod
+    def display_info(self):
+        return self.name
+
+    @abstractmethod
+    def documentation(self):
+        print("""
+                Ceci est la class abstraite person
+              """)
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+        return self.name
+
+class student(person):
+    
+    """Ceci est la classe student qui creer un eleve"""
+    
+    def __init__(self, name, date_of_birth, mark):
+        super().__init__(name, date_of_birth)
+        self.__mark = mark
+        self.__name = name
+        self.create_matricule()
+        self.__date_of_birth = date_of_birth
+        sql.insert_in_student({"name": self.__name, "date_of_birth": self.__date_of_birth, "matricule": self.__matricule, "mark": self.__mark}, None)
+
+    # fonction qui test si le check de doublon marche bien
+    # def get_matricule(self, counter=[0]):
+    #     import string, random
+    #     self.__matricule = ""
+    #     for i in range(8):
+    #         rdm = random.randrange(0, 2)
+    #         if rdm == 0:
+    #             self.__matricule += random.choice(string.ascii_letters)
+    #         elif rdm == 1:
+    #             self.__matricule += str(random.randrange(0,10))
+    #     # print("counter:", counter[0])
+    #     if counter[0] != 0:
+    #         mat = sql.check_matricule(self.__matricule)
+    #         # print("mat:", mat)
+    #         if mat == 1:
+    #             self.get_matricule()
+    #         else:
+    #             return self.__matricule
+    #     else:
+    #         self.__matricule = "0cx8P5z2"
+    #         mat = sql.check_matricule(self.__matricule)
+    #         if mat == 1:
+    #             counter[0] += 1
+    #             self.get_matricule()
+
+    def create_matricule(self):
+        import string, random
+
+        self.__matricule = ""
+
+        for i in range(8):
+            rdm = random.randrange(0, 2)
+            if rdm == 0:
+                self.__matricule += random.choice(string.ascii_letters)
+            elif rdm == 1:
+                self.__matricule += str(random.randrange(0,10))
+            mat = sql.check_matricule(self.__matricule)
+            if mat == 1:
+                self.create_matricule()
+
+    def get_matricule(self):
+        return self.__matricule
+
+    def set_matricule(self, matricule):
+        self.__matricule = matricule
+        return self.__matricule
+
+    def get_name(self):
+        return self.__name
+
+    def set_name(self, name):
+        self.__name = name
+        return self.__name
+
+    def get_date_of_birth(self):
+        return self.__date_of_birth
+
+    def set_date_of_birth(self, date_of_birth):
+        self.__date_of_birth = date_of_birth
+        return self.__date_of_birth
+
+    def get_mark(self):
+        return self.__mark
+    
+    def set_mark(self, mark):
+        self.__mark = mark
+        return self.__mark
+
+    def display_info(self):
+        return self
+
+    def documentation(self):
+        print("""Ceci est la class student""")
+        
+    def modify_student(self, classe):
         os.system('cls')
         while 1:
             entry = input("""Que voulez vous modifier chez l'etudiant:
@@ -58,10 +172,12 @@ class classe():
                         if new_name == "cancel" or new_name == "c":
                             os.system('cls')
                             break
-                        elif re.search("[^a-zA-Zs]", entry):
-                            student.set_name(new_name)
+                        elif re.search("[^a-zA-Zs]", new_name)  is None:
+                            self.__name = new_name
+                            sql.update_in_student("name", str(new_name), self.__matricule())
                             os.system('cls')
                             print("Name changé")
+                            print(sql.get_student())
                             break
                         else:
                             print("Veuillez ecrire un nom valide ou ecriver \"cancel\" ou \"c\" pour annuler")
@@ -81,34 +197,42 @@ class classe():
                             print("Veuillez ecrire une date valide au bon format YYYY-DD-MM")
                             time.sleep(2)
                             continue
-                        student.set_date_of_birth(new_dob)
+                        self.__date_of_birth = new_dob
+                        sql.update_in_student("date_of_birth", str(new_dob), self.__matricule())
                         os.system('cls')
                         print("Date de naissance changé")
+                        print(sql.get_student())
                         break
                 elif int(entry) == 3:
                     while 1:
                         os.system('cls')
-                        new_id = input("Veuillez ecrire le nouveau id ou ecriver \"cancel\" ou \"c\" pour annuler:\n")
-                        if new_id == "cancel" or new_id == "c":
+                        new_matricule = input("Veuillez ecrire le nouveau matricule ou ecriver \"cancel\" ou \"c\" pour annuler:\n")
+                        if new_matricule == "cancel" or new_matricule == "c":
                             os.system('cls')
                             break
-                        elif re.search("[^a-zA-Z0-9]", entry):
-                            student.setId(new_id)
+                        elif re.search("[^a-zA-Z0-9]", new_matricule)  is None:
+                            if sql.check_matricule(str(new_matricule)) == 0:
+                                self.__matricule = new_matricule
+                                sql.update_in_student("matricule", str(new_dob), self.__matricule())
+                            else:
+                                print("Veuillez ecrire un matricule valide ou ecriver \"cancel\" ou \"c\" pour annuler")
+                                continue
                             os.system('cls')
-                            print("id changé")
+                            print("matricule changé")
+                            print(sql.get_student())
                             break
                         else:
-                            print("Veuillez ecrire un id valide ou ecriver \"cancel\" ou \"c\" pour annuler")
+                            print("Veuillez ecrire un matricule valide ou ecriver \"cancel\" ou \"c\" pour annuler")
                             continue
                 elif int(entry) == 4:
                     while 1:
                         os.system('cls')
-                        new_mark = input("Veuillez ecrire sa nouvelle note ou ecriver \"cancel\" ou \"c\" pour annuler:\n")
+                        new_mark = input("Veuillez ecrire sa nouvelle moyenne ou ecriver \"cancel\" ou \"c\" pour annuler:\n")
                         if new_mark == "cancel" or new_mark == "c":
                             os.system('cls')
                             break
                         if new_mark.is_digit():
-                            student.set_mark(new_mark)
+                            self.__mark = new_mark
                             os.system('cls')
                             print("Note changé")
                             break
@@ -117,7 +241,7 @@ class classe():
                             continue
                 elif int(entry) == 5:
                     os.system('cls')
-                    print(self.__students)
+                    sql.list_students()
                     continue
                 elif int(entry) == 6:
                     os.system('cls')
@@ -132,87 +256,153 @@ class classe():
                 print("Veuillez ecrire une option valide ex: 1")
                 continue
 
-    def display_list(self):
-        print(self.__students)
+def school():
+    os.system('cls')
+    while 1:
+        entry = input("""Que voulez vous faire:
+                         [1]: Afficher la liste des Classes
+                         [2]: Afficher la liste des Eleves
+                         [3]: Ajouter une nouvelle Classe
+                         [4]: Ajouter un nouveau/nouvelle Eleve
+                         [5]: Modifier une Classe en particulier
+                         [6]: Modifier un Eleve en particulier
+                         [7]: Voir une demo du programme
+                         [8]: Exit
+                      """)
+        if entry.isdigit():
+            if int(entry) == 1:
+                os.system('cls')
+                sql.list_classes()
+                time.sleep(2)
+            elif int(entry) == 2:
+                os.system('cls')
+                sql.list_students()
+                time.sleep(2)
+            elif int(entry) == 3:
+                while 1:
+                    os.system('cls')
+                    entry = input("Veuillez entrer le nom de la Classe ou ecriver \"cancel\" ou \"c\" pour annuler:\n")
+                    if entry == "cancel" or entry == "c":
+                        os.system('cls')
+                        break
+                    elif re.search("[^a-zA-Zs]", entry)  is None:
+                        entry2 = input("Connaissez-vous la moyenne de la Classe ?\n[1]: Oui\n[2]: Non\n")
+                        if entry2.isdigit():
+                            if int(entry) == 1:
+                                classe = classe(entry, entry2)
+                            elif int(entry) == 1:
+                                classe = classe(entry)
+                                sql.get_classe(entry)
+                            else:
+                                print("Veuillez ecrire une option entre 1 et 2 ex: 1")
+                                continue
+                        else:
+                            print("Veuillez ecrire une option entre 1 et 2 ex: 1")
+                            continue
+                    else:
+                        print("Veuillez ecrire un nom valide ou ecriver \"cancel\" ou \"c\" pour annuler")
+                        continue
+            elif int(entry) == 4:
+                os.system('cls')
+                flag_name = False
+                flag_dob = False
+                flag_mark = False
+                while 1:
+                    if flag_name == True:
+                        break
+                    name = input("Veuillez entrer le nom de l'Eleve ou ecriver \"cancel\" ou \"c\" pour annuler:\n").lower()
+                    if name == "cancel" or name == "c":
+                        os.system('cls')
+                        break
+                    elif re.search("[^a-zA-Zs]", name) is None:
+                        name = name.title()
+                        print("Vous avez entré le nom suivant:", name)
+                        flag_name = True
+                        while 1:
+                            if flag_dob == True:
+                                break
+                            dob = input("Veuillez ecrire la nouvelle date de naissance sous le format YYYY-DD-MM ou ecriver \"cancel\" ou \"c\" pour annuler :\n")
+                            format = "%Y-%d-%m"
+                            if dob == "cancel" or dob == "c":
+                                os.system('cls')
+                                break
+                            try:
+                                datetime.strptime(dob, format)
+                            except ValueError as ve:
+                                os.system('cls')
+                                print("Veuillez ecrire une date valide au bon format YYYY-DD-MM")
+                                time.sleep(2)
+                                continue
+                            print("Vous avez entré la date suivante:", dob)
+                            flag_dob = True
+                            while 1:
+                                if flag_mark == True:
+                                    break
+                                mark = input("Veuillez ecrire sa nouvelle moyenne ou ecriver \"cancel\" ou \"c\" pour annuler:\n")
+                                if mark == "cancel" or mark == "c":
+                                    os.system('cls')
+                                    break
+                                if mark.isdigit():
+                                    print("Vous avez entré la moyenne suivante:", mark)
+                                    stud = student(name, dob, mark)
+                                    print("Generation du matricule de l'etudiant...")
+                                    time.sleep(3)
+                                    print("Generation terminé!")
+                                    time.sleep(2)
+                                    os.system('cls')
+                                    matricule = stud.get_matricule()
+                                    print(sql.get_student(str(matricule)))
+                                    flag_mark = True
+                                else:
+                                    print("Veuillez ecrire une note valide ou ecriver \"cancel\" ou \"c\" pour annuler")
+                                    continue
+                    else:
+                        os.system('cls')
+                        print(name, re.search("[^a-zA-Zs]", name))
+                        print("Veuillez ecrire un nom valide ou ecriver \"cancel\" ou \"c\" pour annuler")
+                        continue
+            elif int(entry) == 6:
+                pass
+            elif int(entry) == 8:
+                os.system('cls')
+                break
+            else:
+                print("Veuillez ecrire une option entre 1 et 8 ex: 7")
+                continue
+        elif entry == "e" or entry == "q" or entry == "quit" or entry == "exit":
+            os.system('cls')
+            break
+        else:
+            print("Veuillez ecrire une option valide ex: 1")
+            continue
 
-from abc import ABC, abstractmethod
-
-class person(ABC):
-    
-    def __init__(self, name, date_of_birth):
-        self.name = name
-        self.date_of_birth = date_of_birth
-    
-    @abstractmethod
-    def display_info(self):
-        return self.name
-    
-    @abstractmethod
-    def documentation(self):
-        print("""
-                Ceci est la class abstraite person
-              """)
-
-    
-class student(person):
-    
-    """Ceci est la classe student qui creer un eleve"""
-    
-    def __init__(self, name, date_of_birth, id, mark):
-        super().__init__(name, date_of_birth)
-        self.id = id
-        self.mark = mark
-        sql.create_student()
-        
-    def get_name(self):
-        return self.name
-    
-    def set_name(self, name):
-        self.name = name
-        return self.name
-
-    def get_date_of_birth(self):
-        return self.date_of_birth
-    
-    def set_date_of_birth(self, date_of_birth):
-        self.date_of_birth = date_of_birth
-        return self.date_of_birth
-    
-    def get_id(self):
-        return self.id
-    
-    def set_id(self, id):
-        self.id = id
-        return self.id
-    
-    def get_mark(self):
-        return self.mark
-    
-    def set_mark(self, mark):
-        self.mark = mark
-        return self.mark
-
-    def display_info(self):
-        return self
-    
-    def documentation(self):
-        print("""Ceci est la class student""")
-    
 if __name__ == '__main__':
     
-    my_class = classe("5eE")
-    print("C'est la classe des:", my_class.name, my_class.average)
+    sql.create_student_table()
+    sql.create_classe_table()
     
-    studentA = student("Patrice", "2023-06-07", 1, 8)
-    studentB = student("Mirah", "2023-06-07", 2, 18)
-    
-    my_class.add_student(studentA)
-    my_class.add_student(studentB)
-    print(my_class.getStudentsList())
-    my_class.remove_student(studentB)
-    print(my_class.getStudentsList())
-    my_class.modify_student(studentA)
-    my_class.calcul_moyenne()
-    my_class.print_average()
-    studentA.documentation()
-    print(studentA.__doc__, end='')
+    school()
+    # classeA = classe("5eE")
+    # classeB = classe("6eE")
+
+    # studentA = student("Patrice", "2023-06-07", 8)
+    # studentB = student("Mirah", "2023-06-07", 18)
+    # studentC = student("Camille", "2023-06-07", 18)
+    # studentD = student("Stan", "2023-06-07", 8)
+
+    # classeA.add_student(studentA)
+    # classeA.add_student(studentC)
+    # classeB.add_student(studentB)
+    # classeB.add_student(studentD)
+    # sql.list_students()
+    # sql.list_classes()
+
+    # my_class.add_student(studentB)
+    # print(my_class.getStudentsList())
+    # my_class.remove_student(studentB)
+    # print(my_class.getStudentsList())
+    # my_class.modify_student(studentA)
+    # my_class.calcul_moyenne()
+    # my_class.print_average()
+    # studentA.documentation()
+    # print(studentA.__doc__, end='')
--- minipro/sql/bdd_request.py
+++ minipro/sql/bdd_request.py
@@ -1,11 +1,11 @@
 import sqlite3
 
 def connexion_bdd():
-    connexion = sqlite3.connect('bdd_school')
+    connexion = sqlite3.connect('sql/bdd_school.db')
     curseur = connexion.cursor()
     return connexion, curseur
 
-def create_student():
+def create_student_table():
     
     connexion, curseur = connexion_bdd()
 
@@ -22,13 +22,12 @@ def create_student():
         date_of_birth TEXT (6, 8)      NOT NULL,
         matricule     TEXT (8, 10)     NOT NULL UNIQUE,
         mark          INTEGER (0, 100) NOT NULL,
-        classe        INTEGER          REFERENCES classe (id) ON DELETE SET NULL
-                                    UNIQUE
+        classe        INTEGER           REFERENCES classe (id) ON DELETE SET NULL
     );
     ''')
     curseur.close()
 
-def create_classe():
+def create_classe_table():
     
     connexion, curseur = connexion_bdd()
     
@@ -41,7 +40,7 @@ def create_classe():
     CREATE TABLE classe (
     id           INTEGER          PRIMARY KEY AUTOINCREMENT
                                   NOT NULL,
-    name         TEXT             NOT NULL
+    name         TEXT (3, 5)    NOT NULL
                                   UNIQUE,
     average_mark INTEGER (0, 100) NOT NULL
     );
@@ -49,24 +48,12 @@ def create_classe():
     ''')
     curseur.close()
 
-def get_student(matricule):
-    
-    connexion, curseur = connexion_bdd()
-    
-    curseur.execute(f'''
-                    SELECT * FROM student WHERE matricule =?
-                    ''', str(matricule),)
-    res = curseur.fetchone()
-    connexion.commit()
-    curseur.close()
-
-    return res
 
 def insert_in_student(data, classe_id):
 
     connexion, curseur = connexion_bdd()
     
-    donnee = (data["name"], data["date_of_birth"], data["id"], data["mark"], classe_id)
+    donnee = (data["name"], data["date_of_birth"], data["matricule"], data["mark"], classe_id)
     
     curseur.execute(f'''
                     INSERT INTO student (name, date_of_birth, matricule, mark, classe) VALUES (?, ?, ?, ?, ?)
@@ -74,56 +61,54 @@ def insert_in_student(data, classe_id):
     connexion.commit()
     curseur.close()
 
-def insert_in_class(table, data):
+def insert_in_classe(data, average=0):
 
     connexion, curseur = connexion_bdd()
     
-    donnee = (data["name"], data["average_mark"])
     
     curseur.execute(f'''
-                    INSERT INTO {table} (name, average_mark) VALUES (?, ?)
-                    ''', donnee)
+                    INSERT INTO classe (name, average_mark) VALUES (?, ?)
+                    ''', (data, average))
     connexion.commit()
     curseur.close()
 
-def delete_from_student(table, matricule):
+def delete_from_student(matricule):
     
     connexion, curseur = connexion_bdd()
     curseur.execute(f"""
-                    DELETE FROM {table} where matricule = ?
+                    DELETE FROM student where matricule = ?
                     """, (str(matricule),))
     connexion.commit()
     curseur.close()
     
-def delete_from_classe(table, name):
+def delete_from_classe(name):
 
     connexion, curseur = connexion_bdd()
     
     curseur.execute(f"""
-                    DELETE FROM {table} where name = ?
+                    DELETE FROM classe where name = ?
                     """, (name,))
     connexion.commit()
     curseur.close()
     
-def update_in_student(table, param, data, matricule):
+def update_in_student(param, data, matricule):
     
     connexion, curseur = connexion_bdd()
 
     curseur.execute(f"""
-                    UPDATE {table} 
+                    UPDATE student 
                     SET {param} = ?
                     where matricule = ?
                     """, (data, matricule))
-
     connexion.commit()
     curseur.close()
 
-def update_in_classe(table, param, data, name):
+def update_in_classe(param, data, name):
     
     connexion, curseur = connexion_bdd()
 
     curseur.execute(f"""
-                    UPDATE {table} 
+                    UPDATE classe 
                     SET {param} = ?
                     where name = ?
                     """, (data, name))
@@ -131,14 +116,128 @@ def update_in_classe(table, param, data, name):
     connexion.commit()
     curseur.close()
 
-# if __name__ == '__main__':
-#     create_classe()
-#     create_student()
-#     insert_in_student('student', {'name': 'John', 'date_of_birth': '1998', 'id': 1, 'mark': 10, 'classe': 1}, 1)
-#     insert_in_student('student', {'name': 'John', 'date_of_birth': '1998', 'id': 2, 'mark': 10, 'classe': 1}, 2)
-#     insert_in_class('classe', {'name': '5eE', 'average_mark': 0})
-#     insert_in_class('classe', {'name': '6eE', 'average_mark': 0})
-#     delete_from_student('student', 1)
-#     delete_from_classe('classe', '5eE')
+def check_matricule(matricule):
+    connexion, curseur = connexion_bdd()
+    curseur.execute(f'''
+                    SELECT matricule FROM student
+                    ''')
+    res = curseur.fetchall()
+    if len(matricule) < 8 or len(matricule) > 10:
+        return 1
+    matricule = f"('{matricule}',)"
+    for mat in res:
+        if str(mat) == matricule:
+            return 1
+    connexion.commit()
+    curseur.close()
+    return 0
+            
+def get_student(matricule):
+    
+    connexion, curseur = connexion_bdd()
+    
+    curseur.execute(f'''
+                    SELECT * FROM student WHERE matricule = ?
+                    ''', (str(matricule),))
+    res = curseur.fetchone()
+    connexion.commit()
+    curseur.close()
+
+    return res
+
+def get_classe(name):
+    
+    connexion, curseur = connexion_bdd()
+    
+    curseur.execute(f'''
+                    SELECT * FROM classe WHERE name = ?
+                    ''', str(name),)
+    res = curseur.fetchone()
+    connexion.commit()
+    curseur.close()
+
+    return res
+
+def list_students():
+    
+    connexion, curseur = connexion_bdd()
+
+    curseur.execute(f"""
+                    SELECT * FROM student
+                    """)
+    resultats = curseur.fetchall()
+    for resultat in resultats:
+        print(resultat)
+    connexion.commit()
+    curseur.close()
+    
+def list_classes():
+
+    connexion, curseur = connexion_bdd()
+
+    curseur.execute(f"""
+                    SELECT * FROM classe
+                    """)
+    resultats = curseur.fetchall()
+    for resultat in resultats:
+        print(resultat)
+    connexion.commit()
+    curseur.close()
+
+def get_classe_id(name):
+    
+    connexion, curseur = connexion_bdd()
+    data = (name, )
+    curseur.execute("""
+                    SELECT id FROM classe where name = ?
+                    """, data)
+    res = curseur.fetchone()
+    connexion.commit()
+    curseur.close()
+
+    return res
+
+def get_all_students():
+    
+    connexion, curseur = connexion_bdd()
+
+    curseur.execute(f"""
+                    SELECT * FROM student
+                    """)
+    resultats = curseur.fetchall()
+    transformed_resultats = []
+    for resultat in resultats:
+        transformed_resultats.append(list(resultat))
+    connexion.commit()
+    curseur.close()
+    return resultats
+
+def get_all_students_from_classe(id):
+    
+    connexion, curseur = connexion_bdd()
+
+    curseur.execute(f"""
+                    SELECT * FROM student where classe = ?
+                    """, (id, ))
+    resultats = curseur.fetchall()
+    transformed_resultats = []
+    for resultat in resultats:
+        transformed_resultats.append(list(resultat))
+    connexion.commit()
+    curseur.close()
+    return resultats
+
+
+
+
+if __name__ == '__main__':
+    # create_classe()
+    # create_student()
+    #
+    # insert_in_classe({'name': '5eE', 'average_mark': 0})
+    # insert_in_classe({'name': '6eE', 'average_mark': 0})
+    # delete_from_student(2)
+    # delete_from_classe('6eE')
 #     update_in_student('student', 'name', 'Camille', 2)
 #     update_in_classe('classe', 'name', '7eE', '6eE')
+    pass
